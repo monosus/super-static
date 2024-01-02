c67e52e9c35c212b2bb437fb2eacf6f7d06ed6bb
monoharada <harada@monosus.co.jp>
2024-01-02 20:56:25 +0900

    feat(evtry.js): 遅延読み込みのステップを追加

--- src/assets/entry.ts
+++ src/assets/entry.ts
@@ -1,5 +1,13 @@
+import { updateDataBg } from "./modules/background-image-lazy-load";
+import { loadScript } from "./modules/load-script";
+
 // loadイベントが完了したらhtmxをimportする
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
+	await loadScript("/assets/ga.js");
+
+	updateDataBg();
+
+	console.table(window.dataLayer);
 	if (
 		document.querySelector(
 			"[hx-boost], [hx-get], [hx-post], [hx-put], [hx-delete], [hx-patch]",
@@ -8,4 +16,8 @@ window.addEventListener("load", () => {
 		import("./modules/htmx");
 		console.log("htmx");
 	}
+});
+
+window.addEventListener("htmx:afterOnLoad", () => {
+	updateDataBg();
 });
