1826fe42dee5884ef41891aa55e8f8eefa4868fa
monoharada <harada@monosus.co.jp>
2024-01-02 20:58:27 +0900

    chore(dummy,tuor): 背景画像のテストページ

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "minista";
 
 import HtmlAppendedLink from "~/components/utils/HtmlAppendedLink";
+import demoHtml from "../../public/assets/demo.html?raw";
 export const metadata: Metadata = {
 	title: "About",
 };
@@ -8,7 +9,12 @@ export const metadata: Metadata = {
 export default function () {
 	return (
 		<>
-			<h1>About</h1>
+			<h1 className="image load-bg" style={{ height: "400vh" }}>
+				About
+			</h1>
+			<h1 className="image-1" data-bg="in-view">
+				About
+			</h1>
 			<HtmlAppendedLink href="/">go Home</HtmlAppendedLink>
 		</>
 	);
