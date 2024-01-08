5aabb844ff7e16708ae8fc009c14fcd4f7c90808
monoharada <harada@monosus.co.jp>
2024-01-08 10:41:25 +0900

    feat(demo): dialogとpopoverのでもを追加

--- src/pages/include/inc.tsx
+++ src/pages/include/inc.tsx
@@ -1,5 +1,60 @@
 const inc = () => {
-	return <div>Enter</div>;
+	return (
+		<>
+			<dialog
+				id="dummy"
+				// popover="auto"
+				// data-modal="true"
+			>
+				<div>
+					<button type="button" data-js-dialog="dummy">
+						close dialog!!
+					</button>
+					<a
+						href="http://"
+						className="lik"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						inner link
+					</a>
+
+					<div>
+						Enter
+						<button
+							type="button"
+							// popoverTargetAction="toggle"
+							// popoverTarget="pop2"
+							data-js-dialog="pop2"
+						>
+							open next modal
+						</button>
+					</div>
+				</div>
+			</dialog>
+			<dialog
+				id="pop2"
+				// popover="manual"
+			>
+				<header>
+					<h2 id="modal-title">include2</h2>
+				</header>
+				body
+				<a href="http://" target="_blank" rel="noopener noreferrer">
+					link
+				</a>
+				<a href="http://" target="_blank" rel="noopener noreferrer">
+					link
+				</a>
+				<footer>
+					footer
+					<button type="button" data-js-dialog="dummy">
+						prev
+					</button>
+				</footer>
+			</dialog>
+		</>
+	);
 };
 
 export default inc;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,63 @@ import HtmlAppendedLink from "~/components/utils/HtmlAppendedLink";
 export const metadata: Metadata = {
 	title: "Home",
 };
+const dialogId = "test";
+const dialogId2 = "dummy";
 
 export default function () {
 	return (
 		<>
 			<h1>index</h1>
+			<button popoverTarget="candle-01" type="button">
+				Quick Shop
+			</button>
+			<div style={{ height: "60vh" }} />
+			<div popover="auto" id="candle-01" data-modal="true">
+				<button
+					type="button"
+					className="close-btn"
+					popoverTarget="candle-01"
+					popoverTargetAction="hide"
+				>
+					...
+				</button>
+				<div className="product-preview-container">...</div>
+				<a href="http://" target="_blank" rel="noopener noreferrer">
+					a
+				</a>
+				<a href="http://" target="_blank" rel="noopener noreferrer">
+					b
+				</a>
+				<a href="http://" target="_blank" rel="noopener noreferrer">
+					c
+				</a>
+			</div>
+
 			<HtmlAppendedLink href="/about">go about</HtmlAppendedLink>
+			<div style={{ height: "60vh" }} />
+			<button
+				type="button"
+				popoverTarget="dummy"
+				data-js-dialog="dummy"
+				data-hx-trigger="load"
+				data-hx-get="/include/inc"
+				data-hx-target="body"
+				data-hx-swap="beforeend"
+			>
+				open dialog
+			</button>
+			<div style={{ height: "60vh" }} />
+			<HtmlAppendedLink href="/about">go about</HtmlAppendedLink>
+			<dialog id={dialogId}>
+				<span>dialog</span>
+				<button type="button" popoverTarget="dummy">
+					close dialog
+				</button>
+				<a href="http://" target="_blank" rel="noopener noreferrer">
+					link
+				</a>
+			</dialog>
+			<span>forfirefox</span>
 		</>
 	);
 }
